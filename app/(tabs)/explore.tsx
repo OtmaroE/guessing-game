2b4f99a554934057ede94fa6c244aba9c30e7efd
Jose Gonzalez <otmaro@outlook.com>
2025-10-12 18:42:21 -0600

    Add base layout for board

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,110 +1,92 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { View, StyleSheet, Pressable } from 'react-native';
+import {useEffect, useState} from 'react';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+function orderGenerator() {
+  const list = [];
+  for (let i = 0; i < 4; i++) {
+    const number = Math.floor(Math.random() * 100) + 1;
+    list.push(number);
+    list.push(number);
+  }
+  list.push(-1);
+  return list.sort(() => Math.random() - 0.5);
+}
 
+function toggleNumber(visibleNumbers: number[], setVisibleNumbers: (numbers: number[]) => void, number: number) {
+  // TODO: Could this be useful to keep track of history?
+  const newSet = new Set(visibleNumbers);
+  if (visibleNumbers.has(number)) {
+    newSet.delete(number);
+  } else {
+    newSet.add(number);
+  }
+  return setVisibleNumbers(newSet);
+}
+
+function evaluate(visibleNumbers: number[], setVisibleNumbers: (numbers: number[]) => void) {
+  if (visibleNumbers.length === 0) return;
+  console.log('Evaluating...');
+  // gather all numner
+  const numbers = [];
+  for (const index of visibleNumbers) {
+    numbers.push(list[index]);
+  };
+  console.log('On board: ', numbers);
+
+}
+// Value to not change for the lifecycle of the component
+const list = orderGenerator();
 export default function TabTwoScreen() {
+  const [visibleNumbers, setVisibleNumbers] = useState(new Set());
+  const [evaluationRound, setEvaluationRound] = useState(true);
+
+  useEffect(() => {
+    if (evaluationRound) evaluate(visibleNumbers, setVisibleNumbers);
+  }, [evaluationRound])
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Explore</ThemedText>
-      </ThemedView>
-      <ThemedText>This app includes example code to help you get started.</ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{' '}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the web version, press{' '}
-          <ThemedText type="defaultSemiBold">w</ThemedText> in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
-        </ThemedText>
-        <Image source={require('@/assets/images/react-logo.png')} style={{ alignSelf: 'center' }} />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText> to see how to load{' '}
-          <ThemedText style={{ fontFamily: 'SpaceMono' }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{' '}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook lets you inspect
-          what the user&apos;s current color scheme is, and so you can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{' '}
-          <ThemedText type="defaultSemiBold">components/HelloWave.tsx</ThemedText> component uses
-          the powerful <ThemedText type="defaultSemiBold">react-native-reanimated</ThemedText>{' '}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+      <View style={styles.container} >
+        {Array.from({ length: 9 }).map((_, idx) => (
+          <Pressable
+            key={idx}
+            onPress={() => {
+              toggleNumber(visibleNumbers, setVisibleNumbers, idx);
+              setEvaluationRound(!evaluationRound);
+            }
+            }
+          >
+            <View key={idx} style={styles.box} >
+              {
+                visibleNumbers.has(idx) &&
+                <div id={`${idx}-number`} style={styles.boxText}>{list[idx]}</div>
+              }
+            </View>
+          </Pressable>
+        ))}
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: '#808080',
-    bottom: -90,
-    left: -35,
-    position: 'absolute',
-  },
-  titleContainer: {
+  container: {
     flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
     gap: 8,
+    padding: 50,
+    width: '100%',
   },
+  box: {
+    width: 80,
+    height: 80,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#132cd9',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  boxText: {
+    fontSize: 18,
+  }
 });
